a4488604d84de40d1c97957985309ddc1a0d3965
elis84-jpg <elis84@nycstudents.net>
2026-05-05 16:07:05 -0400

    Tried to fix console errors

--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -5,25 +5,25 @@ using UnityEngine.UIElements;
 
 public class UIHandler : MonoBehaviour
 {
-    private VisualElement m_Healthbar;    
-    public static UIHandler instance { get; private set; }
- 
-    private void Awake()  
-    {
-        instance = this;
-    }
+	private VisualElement m_Healthbar;
+	public static UIHandler instance {get; private set;}
 
+	//Awake is called when the script instance is being loaded(in this situation, when the game scene loads)
+	private void Awake()
+	{
+		instance = this;
+	}
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        UIDocument uiDocument = GetComponent<UIDocument>();
-        m_Healthbar = uiDocument.rootVisualElement.Q<VisualElement>("HealthBar");
-        SetHealthValue(1.0f);
-    }
+	//Start is called before the first frame update
+	void Start()
+	{
+		UIDocument uiDocument = GetComponent<UIDocument>();
+		m_Healthbar = uiDocument.rootVisualElement.Q<VisualElement>("HealthBar");
+		SetHealthValue(1.0f);
+	}
 
-    public void SetHealthValue(float percentage)
-    {
-        m_Healthbar.style.width = Length.Percent(100 * percentage);
-    }
+	public void SetHealthValue(float percentage)
+	{
+		m_Healthbar.style.width = Length.Percent(100 * percentage);
+	}
 }
